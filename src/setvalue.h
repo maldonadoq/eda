6ab183d191059bd77669c1041290c667cb9993a2
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-03-19 17:52:51 -0500

    value initial

---
+++ src/setvalue.h
@@ -0,0 +1,12 @@
+#ifndef _SETVALUE_H_
+#define _SETVALUE_H_
+
+#include <iostream>
+
+using namespace std;
+
+#define pre 6
+static string cardinal[4] = {"NW","NE","SW","SE"};
+#define m_ndt 20
+
+#endif
